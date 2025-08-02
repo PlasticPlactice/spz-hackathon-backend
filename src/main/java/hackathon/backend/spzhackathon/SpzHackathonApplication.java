0e0d96b285fac7e0570886ddf6c3fdbe77caaa72
yutohatano <y.hatano.sys23@morijyobi.ac.jp>
2025-08-02 11:01:48 +0900

    first commit

---
+++ src/main/java/hackathon/backend/spzhackathon/SpzHackathonApplication.java
@@ -0,0 +1,13 @@
+package hackathon.backend.spzhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpzHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpzHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackathon/backend/spzhackathon/SpzHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.backend.spzhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpzHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
